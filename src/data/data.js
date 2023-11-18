5e048bfb666af30d88cdd83f7824f362749cc053
githTimi <timiagb1@gmail.com>
2023-11-18 19:09:17 +0100

    new work

---
+++ src/data/data.js
@@ -0,0 +1,45 @@
+import Joba from '../assets/projects/Joba.jpg'
+import Ai from '../assets/projects/Ai.png'
+import Nike from '../assets/projects/Nike.png'
+import Summary from '../assets/projects/Summarizer.png'
+import Bank from '../assets/projects/bank.png'
+
+export const data=[
+    {
+        id:1,
+        name:"React JS- Ai Summarizer",
+        image:Summary,
+        github:"https://github.com/githTimi/AI-Summarizer",
+        
+    },
+    {
+        id:2,
+        name:"React Native- Open Ai App",
+        image:Ai,
+        github:"https://github.com/githTimi/Open-Ai-App",
+      
+    },
+    {
+        id:3,
+        name:"React Native- Shoe Store App",
+        image:Nike,
+        github:"https://github.com/githTimi/Shoe-App",
+        
+    },
+    {
+        id:4,
+        name:"React Native- Job App",
+        image:Joba,
+        github:"https://github.com/githTimi/Joba",
+        
+    },
+    {
+        id:5,
+        name:"Zenth Bank App mini clone",
+        image:Bank,
+        github:"https://github.com/githTimi/ZenithBank-MiniClone",
+        
+    },
+
+
+]
